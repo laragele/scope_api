dfd64b1239f8c39034cbadfd3eb60b3a424f1dbc
laragele <142636055+laragele@users.noreply.github.com>
2023-12-26 22:29:35 -0300

    Update Libro.js

--- Libro.js
+++ Libro.js
@@ -1,6 +1,6 @@
 const mongoose=require(`mongoose`);
 
-mongoose.connect(`mongodb://localhost:27017/biblioteca`);
+mongoose.connect(`mongodb://localhost:27017/scope`);
 
 const LibroSchema=new mongoose.Schema({
     titulo:String,
